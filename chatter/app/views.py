fe78b1def6f9b649db8a486ab1e5ae370eea891d
Debian <admin@ip-172-31-27-46.us-west-2.compute.internal>
2022-06-13 03:44:48 +0000

    lab2 back end

--- chatter/app/views.py
+++ chatter/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
+import os, time
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 from django.http import JsonResponse, HttpResponse
@@ -16,6 +19,19 @@ def getchatts(request):
     response['chatts'] = rows
     return JsonResponse(response)
 
+def getimages(request):
+    if request.method != 'GET':
+        return HttpResponse(status=404)
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM images ORDER BY time DESC;')
+    rows = cursor.fetchall()
+
+    response = {}
+    response['chatts'] = rows
+    return JsonResponse(response)
+
+
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -29,4 +45,37 @@ def postchatt(request):
     cursor = connection.cursor()
     cursor.execute('INSERT INTO chatts (username, message) VALUES '
                    '(%s, %s);', (username, message))
+    return JsonResponse({})
+
+@csrf_exempt
+def postimages(request):
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+
+    # loading form-encoded data
+    username = request.POST.get("username")
+    message = request.POST.get("message")
+
+    if request.FILES.get("image"):
+        content = request.FILES['image']
+        filename = username+str(time.time())+".jpeg"
+        fs = FileSystemStorage()
+        filename = fs.save(filename, content)
+        imageurl = fs.url(filename)
+    else:
+        imageurl = None
+
+    if request.FILES.get("video"):
+        content = request.FILES['video']
+        filename = username+str(time.time())+".mp4"
+        fs = FileSystemStorage()
+        filename = fs.save(filename, content)
+        videourl = fs.url(filename)
+    else:
+        videourl = None
+
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO images (username, message, imageurl, videourl) VALUES '
+                   '(%s, %s, %s, %s);', (username, message, imageurl, videourl))
+
     return JsonResponse({})
--- chatter/routing/settings.py
+++ chatter/routing/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT= BASE_DIR/'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = 'https://52.39.198.75/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
